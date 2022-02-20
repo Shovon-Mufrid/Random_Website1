b1801a602225a40546a340524a4dcfd4112059b0
Shovon Mufrid <shovonmufrid98@gmail.com>
2022-02-20 10:48:54 +0600

    Update Database.php

--- Database.php
+++ Database.php
@@ -7,8 +7,23 @@ $db_name = 'web course';
 
 $db = mysqli_connect($db_host, $db_uname, $db_pass, $db_name);
 
-function shogreetings(){
-    return "Hello I am Shovon";
+function db_connect(){
+    $db_host = 'localhost';
+    $db_uname = 'root';
+    $db_pass = '';
+    $db_name = 'web course';   
+    return mysqli_connect($db_host, $db_uname, $db_pass, $db_name);
+}
+
+function get_all($table_name){
+    $get_query = "SELECT * FROM $table_name ";
+    return mysqli_query(db_connect(),$get_query);
+}
+
+function get_all_active($table_name,$status = 1,$order = 'ASC'){
+    $get_all_active_query = "SELECT * FROM $table_name 
+                            WHERE active_status = $status ORDER BY id $order";
+    return mysqli_query(db_connect(),$get_all_active_query);
 }
 
 
